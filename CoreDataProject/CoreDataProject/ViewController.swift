ac26d12b196f52eceed86949a3dd059895257b96
Данил <danil.ryabikov@bk.ru>
2022-02-13 17:31:16 +0300

    Initial xcode commit

---
+++ CoreDataProject/CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Данил on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
